413be1834b02dc11e98b562ebf1d7c58fd0628cb
rakhmatullina <rakhmatullina.99@yandex.ru>
2023-05-22 22:29:04 +0300

    Написаны модели категорий, жанров и произведений

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Модель категорий произведений"""
+    name = models.CharField(
+        'Категория',
+        max_length=256
+    )
+    slug = models.SlugField(
+        'Слаг',
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    """Модель жанров"""
+    name = models.CharField(
+        'Жанр',
+        max_length=256
+    )
+    slug = models.SlugField(
+        'Слаг',
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    """Модель произведений"""
+    name = models.CharField(
+        'Произведение',
+        max_length=256
+    )
+    description = models.CharField(
+        'Описание произведения',
+        max_length=256
+    )
+    year = models.IntegerField(
+        'Год выпуска произведения'
+    )
+    genre = models.ManyToManyField(
+        Genres,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        related_name='category',
+        on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.name
