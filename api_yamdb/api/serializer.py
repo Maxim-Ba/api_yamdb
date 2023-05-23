e2501b0334f959804feb6d6201ad4a551748887a
Khaliullin Ruslan <mhaliullin@gmail.com>
2023-05-23 23:13:44 +0300

    Add some changes

--- api_yamdb/api/serializer.py
+++ api_yamdb/api/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from reviews.models import Review
+from reviews.models import Comment, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -19,4 +19,19 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ('__all__',)
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only =True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from reviews.models import Comment, Title
+from .serializer import ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+
+    def get_queryset(self):
+        pk = self.kwargs.get('title_id')
+        review_queryset = get_object_or_404(Title, pk=pk)
+        return review_queryset.reviews.all()
