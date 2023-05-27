21a6cd19212622fb3aea2d146f844264d6becba1
Maxim Balashov <79164211428@yandex.ru>
2023-05-28 00:40:27 +0300

    Тесты пройдены.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,15 +17,13 @@ class IsAdmin(BasePermission):
     message = "Вы не администратор."
 
     def has_permission(self, request, view):
-        return (
-            request.user
-            and request.user.is_authenticated
-            and request.user.role == "admin"
+        return request.user and (
+            (request.user.is_authenticated and request.user.role == "admin")
             or request.user.is_superuser
         )
 
     def has_object_permission(self, request, view, obj):
-        return request.user.role == "admin"
+        return request.user.role == "admin" or request.user.is_superuser
 
 
 class IsModerator(BasePermission):
