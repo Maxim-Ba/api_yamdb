7a85cbeb69af132eb82b7247c0caa24e10cb8748
Khaliullin Ruslan <mhaliullin@gmail.com>
2023-05-28 15:16:46 +0300

    add some changes

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,20 @@
-from rest_framework import routers
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.routers import DefaultRouter
 
+from .views import UserViewSet, signup, token, CommentViewSet, ReviewViewSet
 
-from .views import UserViewSet, signup, token
-
-router_v1 = routers.SimpleRouter()
-router_v1.register(r"users", UserViewSet, basename="users")
-
+router_v1 = DefaultRouter()
+router_v1.register(r'users', UserViewSet, basename='users')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
