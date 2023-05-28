5b5d41a3042001d94b75cab1ae31c5f70692917b
Khaliullin Ruslan <mhaliullin@gmail.com>
2023-05-28 14:27:18 +0300

    Попробуй слить ветки

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.core.validators import validate_email
 
-from reviews.models import User
+from reviews.models import User, Comment, Review
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -82,3 +82,43 @@ def _validate_email(value):
     if validate_email(value.lower()):
         raise ValidationError(detail="Не корректный емайл")
     return value
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context.get['request']
+        if request.method == 'POST':
+            title = data['title']
+            if title.reviews.filter(author=request.user).exists():
+                raise serializers.ValidationError(
+                    'На это произведение вы уже оставляли отзыв.'
+                )
+        return data
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
